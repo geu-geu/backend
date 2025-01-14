dfa47dfd058b9e8612141e7cc354273acc642113
Jihwan Lim <ghwn5936@gmail.com>
2025-01-14 20:05:29 +0900

    ValidationPipe 설정

--- projects/geugeu/src/main.ts
+++ projects/geugeu/src/main.ts
@@ -1,10 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { INestApplication } from '@nestjs/common';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   setupSwagger(app);
   await app.listen(process.env.PORT ?? 3000);
 }
