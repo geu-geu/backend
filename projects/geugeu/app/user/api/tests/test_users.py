53e4de1332c06c1e8e0bf55b74010d44afe17763
Jihwan Lim <ghwn5936@gmail.com>
2025-03-29 22:41:08 +0900

    user api 테스트 수정

--- projects/geugeu/app/user/api/tests/test_users.py
+++ projects/geugeu/app/user/api/tests/test_users.py
@@ -1,46 +1,37 @@
 from datetime import UTC, datetime
-from unittest.mock import patch
 
+import pytest
 from fastapi.testclient import TestClient
+from sqlmodel import Session
 from ulid import ULID
 
 from app.auth.dependencies import get_current_active_user
+from app.database import get_db
 from app.main import app
-from app.user.application.user_service import UserService
 from app.user.domain.user import User
+from app.user.domain.user_repository import IUserRepository
+from app.user.infrastructure.user_repository import UserRepository
 
 client = TestClient(app)
 
 
-@patch.object(UserService, "signup")
-def test_signup(signup):
-    # given
-    payload = {"email": "user@example.com", "password": "P@ssw0rd1234"}
+@pytest.fixture(autouse=True)
+def mock_session(session: Session):
+    def override_get_db():
+        yield session
 
-    user = User(
-        id=str(ULID()),
-        email=payload["email"],
-        name=None,
-        password=payload["password"],
-        is_admin=False,
-        is_active=True,
-        created_at=datetime.now(UTC),
-        updated_at=datetime.now(UTC),
-    )
-    signup.return_value = user
-
-    # when
-    response = client.post("/api/users/signup", json=payload)
-
-    # then
-    assert response.status_code == 201
-    assert response.json()["id"] == user.id
-    assert response.json()["email"] == user.email
+    app.dependency_overrides[get_db] = override_get_db
+    yield
+    app.dependency_overrides.clear()
 
 
-@patch.object(UserService, "get_user")
-def test_me(get_user):
-    # given
+@pytest.fixture()
+def user_repository():
+    return UserRepository()
+
+
+@pytest.fixture()
+def current_active_user():
     user = User(
         id=str(ULID()),
         email="user@example.com",
@@ -48,19 +39,40 @@ def test_me(get_user):
         password="password",
         is_admin=False,
         is_active=True,
+        is_verified=False,
+        profile_image_url=None,
         created_at=datetime.now(UTC),
         updated_at=datetime.now(UTC),
     )
-    get_user.return_value = user
 
     def override_get_current_active_user():
         return user
 
     app.dependency_overrides[get_current_active_user] = override_get_current_active_user
+    yield user
+    app.dependency_overrides.clear()
+
+
+def test_signup():
+    # given
+    payload = {"email": "user@example.com", "password": "P@ssw0rd1234"}
+
+    # when
+    response = client.post("/api/users/signup", json=payload)
+
+    # then
+    assert response.status_code == 201
+    assert response.json()["email"] == payload["email"]
+
+
+def test_me(
+    session: Session, user_repository: IUserRepository, current_active_user: User
+):
+    # given
+    user_repository.save(session, current_active_user)
 
     # when
     response = client.get("/api/users/me")
 
     # then
     assert response.status_code == 200
-    assert response.json()["id"] == user.id
