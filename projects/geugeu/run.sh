e6ebddb43c0a9e6e14f3479b7ace2805bee5cc71
Jihwan Lim <ghwn5936@gmail.com>
2025-04-05 16:42:21 +0900

    Dockerfile 수정
    poetry 대신 uv 사용

--- projects/geugeu/run.sh
+++ projects/geugeu/run.sh
@@ -1,4 +1,4 @@
 set -e
 
-poetry run alembic upgrade head
-poetry run uvicorn app.main:app --host 0.0.0.0 --port 8000
+uv run alembic upgrade head
+uv run uvicorn app.main:app --host 0.0.0.0 --port 8000
