1be302070f69f53cd65fb1e609c1fe4cf343ebaa
Jihwan Lim <ghwn5936@gmail.com>
2025-05-02 11:14:52 +0900

    Image, Comment 테이블 정의

--- projects/geugeu/app/models.py
+++ projects/geugeu/app/models.py
@@ -163,3 +163,48 @@ class DrawingComment(Base):
         onupdate=func.now(),
     )
     deleted_at: Mapped[datetime | None] = mapped_column(DateTime(timezone=True))
+
+
+class Image(Base):
+    __tablename__ = "image"
+
+    id: Mapped[int] = mapped_column(BigInteger(), Identity(), primary_key=True)
+    code: Mapped[str] = mapped_column(String(255), unique=True, nullable=False)
+    post_id: Mapped[int | None] = mapped_column(BigInteger(), nullable=True)
+    drawing_id: Mapped[int | None] = mapped_column(BigInteger(), nullable=True)
+    url: Mapped[str] = mapped_column(String(2083), nullable=False)
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        nullable=False,
+        server_default=func.now(),
+    )
+    updated_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        nullable=False,
+        server_default=func.now(),
+        onupdate=func.now(),
+    )
+    deleted_at: Mapped[datetime | None] = mapped_column(DateTime(timezone=True))
+
+
+class Comment(Base):
+    __tablename__ = "comment"
+
+    id: Mapped[int] = mapped_column(BigInteger(), Identity(), primary_key=True)
+    code: Mapped[str] = mapped_column(String(255), unique=True, nullable=False)
+    author_id: Mapped[int] = mapped_column(BigInteger(), nullable=False)
+    post_id: Mapped[int | None] = mapped_column(BigInteger(), nullable=True)
+    drawing_id: Mapped[int | None] = mapped_column(BigInteger(), nullable=True)
+    content: Mapped[str] = mapped_column(Text, nullable=False)
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        nullable=False,
+        server_default=func.now(),
+    )
+    updated_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        nullable=False,
+        server_default=func.now(),
+        onupdate=func.now(),
+    )
+    deleted_at: Mapped[datetime | None] = mapped_column(DateTime(timezone=True))
