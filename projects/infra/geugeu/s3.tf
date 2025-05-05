d2cd23d9058c2be66871e7329860d01a6b56cd1d
Jihwan Lim <ghwn5936@gmail.com>
2025-05-06 00:28:51 +0900

    chore: s3 권한 설정

--- projects/infra/geugeu/s3.tf
+++ projects/infra/geugeu/s3.tf
@@ -6,9 +6,9 @@ resource "aws_s3_bucket_public_access_block" "geugeu" {
   bucket = aws_s3_bucket.geugeu.id
 
   block_public_acls       = true
-  block_public_policy     = true
+  block_public_policy     = false
   ignore_public_acls      = true
-  restrict_public_buckets = true
+  restrict_public_buckets = false
 }
 
 resource "aws_s3_bucket_versioning" "geugeu" {
@@ -17,4 +17,24 @@ resource "aws_s3_bucket_versioning" "geugeu" {
   versioning_configuration {
     status = "Enabled"
   }
+}
+
+data "aws_iam_policy_document" "allow_s3_access" {
+  statement {
+    principals {
+      type        = "AWS"
+      identifiers = ["*"]
+    }
+    actions = [
+      "s3:GetObject",
+    ]
+    resources = [
+      "${aws_s3_bucket.geugeu.arn}/images/*",
+    ]
+  }
+}
+
+resource "aws_s3_bucket_policy" "allow_s3_access" {
+  bucket = aws_s3_bucket.geugeu.id
+  policy = data.aws_iam_policy_document.allow_s3_access.json
 }
