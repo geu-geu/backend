a9dcc71295125dff77add35cb7161140d24b4c90
Jihwan Lim <ghwn5936@gmail.com>
2025-05-10 22:12:41 +0900

    chore: ECR 이미지 개수를 최근 30개로 유지

--- projects/infra/geugeu/ecr.tf
+++ projects/infra/geugeu/ecr.tf
@@ -1,3 +1,24 @@
 resource "aws_ecr_repository" "main" {
   name = local.project
 }
+
+resource "aws_ecr_lifecycle_policy" "main" {
+  repository = aws_ecr_repository.main.name
+
+  policy = jsonencode({
+    rules = [
+      {
+        rulePriority = 1
+        description  = "Keep last 30 images"
+        selection = {
+          tagStatus   = "untagged"
+          countType   = "imageCountMoreThan"
+          countNumber = 30
+        }
+        action = {
+          type = "expire"
+        }
+      }
+    ]
+  })
+}
